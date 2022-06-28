294ec54489a801ca54d038f77df548a9144d6fb8
Jesus <jesus.fdz93@gmail.com>
2022-06-27 21:44:26 -0500

    proyecto iniciado

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Footer />
+      <NavBar />
+      <Title />
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+
+const HomePages = () => {
+  return (
+    <>
+      <Footer />
+      <NavBar />
+      <Title />
+    </>
+  );
+};
+
+export default HomePages;
